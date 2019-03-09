91392a625952bb29849d3f227368d33b81555aef
BlackBearGames <33421921+JulioYagami@users.noreply.github.com>
2019-03-09 16:47:11 -0300

    unit class setget methods

--- units/unit.gd
+++ units/unit.gd
@@ -1,6 +1,21 @@
 extends Node
 
-var unit_name
+# here are all the common units' features
+
+var unit_name: String = "unit_name" setget set_unit_name, get_unit_name
+var unit_nation: String = "unit_nation" setget set_unit_nation, get_unit_nation
 
 func _ready():
 	pass
+
+func set_unit_name(new_unit_name: String) -> void:
+	unit_name = new_unit_name
+
+func get_unit_name() -> String:
+	return unit_name
+
+func set_unit_nation(new_unit_nation: String) -> void:
+	unit_nation = new_unit_nation
+
+func get_unit_nation() -> String:
+	return unit_nation
